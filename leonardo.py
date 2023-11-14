86720d4dc89fd6a9952cc00c0dba48b5bfdb4394
nickpodratz <nick@podratz.de>
2023-11-14 15:43:51 +0100

    Adjust argparse arguments

--- leonardo.py
+++ leonardo.py
@@ -76,15 +76,17 @@ def parse_args():
         '-p',
         '--prev',
         type=int,
-        help='number of preceding values',
-        default=0
+        nargs='?',
+        default=0,
+        help='number of preceding values'
     )
     parser.add_argument(
         '-n',
         '--next',
         type=int,
-        help='number of succeeding values',
-        default=0
+        nargs='?',
+        default=1,
+        help='number of succeeding values'
     )
     args = parser.parse_args()
     return args
@@ -93,8 +95,8 @@ def main():
     args = parse_args()
     metal = args.metal  # assume Gold
     scale_factor = args.scale or 1
-    start = -args.prev + 1
-    stop = args.next + 2
+    start = 0 if args.prev is None else -args.prev + 1
+    stop = 2 if args.next is None else args.next + 1
     golden_numbers = GoldenSequence(scale_factor)
     golden_strings = map(str, golden_numbers[start:stop])
     output = '\n'.join(golden_strings)
