42de3e79a33bfdf6df057b67ba624fdac293ebd1
nickpodratz <nick@podratz.de>
2023-11-28 00:18:29 +0100

    Change order of methods inside Metal class

--- leonardo/metals/metal.py
+++ leonardo/metals/metal.py
@@ -6,9 +6,6 @@ from ..utils.geometric_sequence import GeometricSequence
 class Metal(ABC):
     """A class to simplify working with the metallic means."""
 
-    def __init__(self, scale_factor: float = 1.0) -> None:
-        self._scale_factor = scale_factor
-
     @property
     @classmethod
     @abstractmethod
@@ -16,14 +13,17 @@ class Metal(ABC):
         """A metallic ratio."""
         raise NotImplementedError
 
+    @classmethod
+    def sequence(cls, scale_factor: float = 1.0) -> GeometricSequence:
+        """A series of numbers following the metallic ratio in growth."""
+        return GeometricSequence(common_ratio=cls.ratio, scale_factor=scale_factor)
+
+    def __init__(self, scale_factor: float = 1.0) -> None:
+        self._scale_factor = scale_factor
+
     def __getitem__(self, subscript) -> list[float]:
         return type(self).sequence(scale_factor=self._scale_factor)[subscript]
 
     def __call__(self, n: int = 1) -> float:
         [item] = self[n]
         return item
-
-    @classmethod
-    def sequence(cls, scale_factor: float = 1.0) -> GeometricSequence:
-        """A series of numbers following the metallic ratio in growth."""
-        return GeometricSequence(common_ratio=cls.ratio, scale_factor=scale_factor)
