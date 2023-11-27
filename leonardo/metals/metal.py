5f99e588709daf9517c2012dae629b142c728634
nickpodratz <nick@podratz.de>
2023-11-27 23:47:25 +0100

    Allow instantiation to Metal subclasses and add subscript access

--- leonardo/metals/metal.py
+++ leonardo/metals/metal.py
@@ -6,12 +6,18 @@ from ..utils.geometric_sequence import GeometricSequence
 class Metal(ABC):
     """A class to simplify working with the metallic means."""
 
+    def __init__(self, scale_factor: float = 1.0) -> None:
+        self._scale_factor = scale_factor
+
     @property
     @classmethod
     @abstractmethod
     def ratio(cls) -> float:
         """A metallic ratio."""
         raise NotImplementedError
+
+    def __getitem__(self, subscript) -> list[float]:
+        return type(self).sequence(scale_factor=self._scale_factor)[subscript]
 
     @classmethod
     def sequence(cls, scale_factor: float = 1.0) -> GeometricSequence:
