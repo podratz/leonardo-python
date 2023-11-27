98eff4a5c914b105f0fb03a88a9463839ddf03b1
nickpodratz <nick@podratz.de>
2023-11-28 00:57:57 +0100

    Implement < and total ordering for Metal

--- leonardo/metals/metal.py
+++ leonardo/metals/metal.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
+from functools import total_ordering
 from types import NotImplementedType
 
 from ..utils.geometric_sequence import GeometricSequence
 
 
+@total_ordering
 class Metal(ABC):
     """A class to simplify working with the metallic means."""
 
@@ -36,6 +38,11 @@ class Metal(ABC):
             type(self).ratio == type(other).ratio
             and self._scale_factor == other._scale_factor
         )
+
+    def __lt__(self, other: object) -> NotImplementedType | bool:
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        return self._scale_factor < other._scale_factor
 
     def __repr__(self) -> str:
         cls = self.__class__
