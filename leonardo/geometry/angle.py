e623edef280473f39d453007bb71c716944d078a
nickpodratz <nick@podratz.de>
2023-12-16 14:15:25 +0100

    Add Angle.Measure enum and simplify code

--- leonardo/geometry/angle.py
+++ leonardo/geometry/angle.py
@@ -1,8 +1,13 @@
 import math
+from enum import Enum, auto
 from typing import Self
 
 
 class Angle:
+    class Measure(Enum):
+        degrees = auto()
+        radians = auto()
+
     def __init__(self, radians: float) -> None:
         self.radians = radians
 
@@ -14,3 +19,11 @@ class Angle:
     @property
     def degrees(self) -> float:
         return self.radians * 360 / math.tau
+
+    @classmethod
+    def divisor(cls, measure: Measure = Measure.radians) -> float:
+        return 360 if measure is Angle.Measure.radians else math.tau
+
+    @classmethod
+    def from_mean(cls, mean: float, measure: Measure = Measure.radians):
+        return cls(cls.divisor(measure) * mean)
--- leonardo/sequences/angle_sequence.py
+++ leonardo/sequences/angle_sequence.py
@@ -1,24 +1,22 @@
-import math
 from typing import cast
 
+from ..geometry import Angle
 from .arithmetic_sequence import ArithmeticSequence
 
 
 class AngleSequence(ArithmeticSequence):
     """An arithmetic sequence that revolves."""
 
-    def __init__(self, angle: float, degrees=False, revolves=False) -> None:
+    def __init__(
+        self, angle: float, measure=Angle.Measure.radians, revolves=False
+    ) -> None:
         super().__init__(common_difference=angle, initial_term=0.0)
-        self.degrees = degrees
+        self.measure = measure
         self.revolves = revolves
-
-    @property
-    def _divisor(self) -> float:
-        return 360 if self.degrees else math.tau
 
     def __getitem__(self, subscript: int | slice) -> float | list[float]:
         item = super().__getitem__(subscript)
-        if isinstance(subscript, int):
-            angle = cast(float, item)
-            item = angle if self.revolves else angle % self._divisor
+        if isinstance(subscript, int) and self.revolves:
+            item = cast(float, item)
+            item %= Angle.divisor(self.measure)
         return item
