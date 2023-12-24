e904e4be0b6a661bba86e915739fe14ec36c1a04
nickpodratz <nick@podratz.de>
2023-12-24 16:36:49 +0100

    Replace invalid inheritance with composition

--- leonardo/sequences/anglesequence.py
+++ leonardo/sequences/anglesequence.py
@@ -5,11 +5,13 @@ from ..geometry import Angle
 from .arithmeticsequence import ArithmeticSequence
 
 
-class AngleSequence(ArithmeticSequence):
+class AngleSequence:
     """An arithmetic sequence of angles."""
 
     def __init__(self, angle: Angle, start: Angle = Angle.zero()) -> None:
-        super().__init__(common_difference=angle.radians, initial_term=start.radians)
+        self.sequence = ArithmeticSequence(
+            common_difference=angle.radians, initial_term=start.radians
+        )
 
     @overload
     def __getitem__(self, subscript: int) -> Angle:
@@ -20,7 +22,7 @@ class AngleSequence(ArithmeticSequence):
         ...
 
     def __getitem__(self, subscript: int | slice) -> Angle | list[Angle]:
-        item = super().__getitem__(subscript)
+        item = self.sequence[subscript]
         if isinstance(subscript, int):
             radians = cast(float, item)
             return Angle.from_radians(radians)
@@ -31,12 +33,12 @@ class AngleSequence(ArithmeticSequence):
     def __add__(self, other: object) -> NotImplementedType | Self:
         if isinstance(other, Angle):
             cls = type(self)
-            angle = Angle.from_radians(self.common_difference)
-            start = Angle.from_radians(self.initial_term + other.radians)
+            angle = Angle.from_radians(self.sequence.common_difference)
+            start = Angle.from_radians(self.sequence.initial_term + other.radians)
             return cls(angle=angle, start=start)
         return NotImplemented
 
     def __iadd__(self, other: object) -> NotImplementedType | None:
         if isinstance(other, Angle):
-            self.initial_term += other.radians
+            self.sequence.initial_term += other.radians
         return NotImplemented
