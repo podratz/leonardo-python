8ea6dbc2f2622efdfadc296c975996577fa0be65
nickpodratz <nick@podratz.de>
2024-02-07 02:39:22 +0100

    Add ratio string conversion with rounding

--- leonardo/geometry/ratio.py
+++ leonardo/geometry/ratio.py
@@ -5,7 +5,10 @@ from dataclasses import dataclass
 class Ratio:
     """Wrapper that provides more semantic interface to mathematical ratio concept."""
 
-    value: int = 1
+    value: int | float = 1
 
     def __eq__(self, value: object) -> bool:
         return self.value == value
+
+    def __str__(self) -> str:
+        return f"{self.value:.3f}"
