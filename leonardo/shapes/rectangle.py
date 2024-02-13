18e5869c4c4bb668b120019bebe89ccde5e7d6e4
nickpodratz <nick@podratz.de>
2024-02-13 13:19:03 +0100

    Add rectangle initialization

--- leonardo/shapes/rectangle.py
+++ leonardo/shapes/rectangle.py
@@ -1,2 +1,5 @@
 class Rectangle:
-    pass
+    def __init__(self, width: float, height: float) -> None:
+        """Create a rectangle."""
+        self.width = width
+        self.height = height
--- tests/shapes/test_rectangle.py
+++ tests/shapes/test_rectangle.py
@@ -4,9 +4,12 @@ from leonardo.shapes.rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
-    def test_rectangle(self) -> None:
-        rectangle = Rectangle()
-        self.assertIsNot(rectangle, None)
+    def test_create_rectangle(self) -> None:
+        width = 10
+        height = 20
+        rectangle = Rectangle(width, height)
+        self.assertEqual(rectangle.width, width)
+        self.assertEqual(rectangle.height, height)
 
 
 if __name__ == "__main__":
