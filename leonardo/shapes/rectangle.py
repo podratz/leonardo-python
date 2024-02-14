a1ba0648192b7ad9df181a768e75178aed795811
nickpodratz <nick@podratz.de>
2024-02-14 22:09:53 +0100

    Add docstrings to Rectangle methods

--- leonardo/shapes/rectangle.py
+++ leonardo/shapes/rectangle.py
@@ -1,13 +1,17 @@
 class Rectangle:
+    """A class that encapsulates logic around the Rectangle shape."""
+
     def __init__(self, width: float, height: float) -> None:
         """Create a rectangle."""
         self.width = width
         self.height = height
 
     def __eq__(self, other: object) -> bool:
+        """Test rectangle for equality."""
         if not isinstance(other, Rectangle):
             return NotImplemented
         return self.width == other.width and self.height == other.height
 
     def __repr__(self) -> str:
+        """Serialize rectangle for us programmers."""
         return f"Rectangle(width={self.width}, height={self.height})"
