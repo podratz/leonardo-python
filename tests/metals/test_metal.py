f52f0b07c179abcf7eb5450308384c77c8ce1990
nickpodratz <nick@podratz.de>
2024-02-14 19:21:39 +0100

    Simplify metallic rectangle test-cases

--- tests/metals/test_metal.py
+++ tests/metals/test_metal.py
@@ -17,18 +17,6 @@ class TestMetal(unittest.TestCase):
 
     # Rectangle
 
-    def test_metallic_rectangle_of_height_negative_3(self) -> None:
-        dneg3 = DoubleMetal(-3)
-        dneg3_rectangle = dneg3.rectangle()
-        self.assertEqual(dneg3_rectangle.height, -3)
-        self.assertEqual(dneg3_rectangle.width, -6)
-
-    def test_metallic_rectangle_of_height_negative_1(self) -> None:
-        dneg1 = DoubleMetal(-1)
-        dneg1_rectangle = dneg1.rectangle()
-        self.assertEqual(dneg1_rectangle.height, -1)
-        self.assertEqual(dneg1_rectangle.width, -2)
-
     def test_metallic_rectangle_of_height_zero(self) -> None:
         d0 = DoubleMetal(0)
         d0_rectangle = d0.rectangle()
@@ -41,11 +29,11 @@ class TestMetal(unittest.TestCase):
         self.assertEqual(d1_rectangle.height, 1)
         self.assertEqual(d1_rectangle.width, 2)
 
-    def test_metallic_rectangle_of_height_three(self) -> None:
-        d3 = DoubleMetal(3)
-        d3_rectangle = d3.rectangle()
-        self.assertEqual(d3_rectangle.height, 3)
-        self.assertEqual(d3_rectangle.width, 6)
+    def test_metallic_rectangle_of_height_negative_1_point_5(self) -> None:
+        dneg15 = DoubleMetal(-1.5)
+        dneg15_rectangle = dneg15.rectangle()
+        self.assertEqual(dneg15_rectangle.height, -1.5)
+        self.assertEqual(dneg15_rectangle.width, -3)
 
 
 if __name__ == "__main__":
